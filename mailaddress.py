a0982f36eb716e089c284b5c991fb193f56e894f
parisa-a <parisa.acharya22@ycdsbk12.ca>
2020-03-24 13:37:46 -0400

    Several variables and lines of printing were added to print my address.

---
+++ mailaddress.py
@@ -0,0 +1,10 @@
+name = "PARISA ACHARYA"
+street = "29 MORESBY ST"
+city = "RICHMOND HILL"
+province = "ON"
+postal_code = "L4B 4K9"
+country = "CANADA"
+print(name)
+print(street)
+print (city + " " + province + " " + postal_code)
+print(country)
