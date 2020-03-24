58c977002232e1a026edf7cdd80d1625b3803322
parisa-a <parisa.acharya22@ycdsbk12.ca>
2020-03-24 15:17:57 -0400

    Steps taken to create the code were added; useful for future reference

--- mailaddress.py
+++ mailaddress.py
@@ -1,9 +1,15 @@
+## author: parisa acharya
+## exercise: print mailing address to screen
+##
+## set variables with text
 name = "PARISA ACHARYA"
 street = "29 MORESBY ST"
 city = "RICHMOND HILL"
 province = "ON"
 postal_code = "L4B 4K9"
 country = "CANADA"
+##
+## print above variables to screen 
 print(name)
 print(street)
 print (city + " " + province + " " + postal_code)
